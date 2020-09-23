a513d202794c942d70c752622df0d74d5f9b5b7c
TautvydasGec <gecevicius.tautvydas@gmail.com>
2020-09-23 11:46:26 +0200

    Setting up Api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>
-      hello world
-    </div>
-  )
-}
+import {
+  Cards,
+  Chart,
+  CountryPicker,
+} from './components'
 
+import { fetchData } from './api'
+
+import styles from './App.module.css'
+
+class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData()
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
+}
 export default App
 
